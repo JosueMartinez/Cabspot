69b6abbb5513b6869ffb3296b9c4c8f77f42cb9a
Josue Martinez <jmh_3@hotmail.com>
2015-10-28 15:57:34 -0400

    Adding Base(Web-Api) Controller

---
+++ Cabspot/basesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Cabspot.Models;
+
+namespace Cabspot
+{
+    public class basesController : ApiController
+    {
+        private CabspotDB db = new CabspotDB();
+
+        // GET: api/bases
+        public IQueryable<bases> Getbases()
+        {
+            return db.bases;
+        }
+
+        // GET: api/bases/5
+        [ResponseType(typeof(bases))]
+        public async Task<IHttpActionResult> Getbases(int id)
+        {
+            bases bases = await db.bases.FindAsync(id);
+            if (bases == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bases);
+        }
+
+        // PUT: api/bases/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putbases(int id, bases bases)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bases.idBase)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(bases).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!basesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/bases
+        [ResponseType(typeof(bases))]
+        public async Task<IHttpActionResult> Postbases(bases bases)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.bases.Add(bases);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = bases.idBase }, bases);
+        }
+
+        // DELETE: api/bases/5
+        [ResponseType(typeof(bases))]
+        public async Task<IHttpActionResult> Deletebases(int id)
+        {
+            bases bases = await db.bases.FindAsync(id);
+            if (bases == null)
+            {
+                return NotFound();
+            }
+
+            db.bases.Remove(bases);
+            await db.SaveChangesAsync();
+
+            return Ok(bases);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool basesExists(int id)
+        {
+            return db.bases.Count(e => e.idBase == id) > 0;
+        }
+    }
+}
