068f5bb9874582593f50cb3b91a444dfecf1086d
JosueMartinez <jmh_3@hotmail.com>
2015-11-14 16:22:07 -0400

    cambio de prueba git

---
+++ Cabspot/Controllers/vehiculosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cabspot.Controllers
+{
+    public class vehiculosController : Controller
+    {
+        // GET: vehiculos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
