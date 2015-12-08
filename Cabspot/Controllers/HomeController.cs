a020ae011d90ee462cf10d5ee4f7f876223977a8
JosueMartinez <jmh_3@hotmail.com>
2015-12-08 11:50:27 -0400

    ubicacion del taxista en el detalle

--- Cabspot/Controllers/HomeController.cs
+++ Cabspot/Controllers/HomeController.cs
@@ -20,6 +20,19 @@ namespace Cabspot.Controllers
                 return Json(taxistas.ToList(), JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult ubicacionTaxista(int id)
+        {
+            
+            if (id != null)
+            {
+                var taxistas = from t in db.taxistas where t.idTaxista == id select new {t.codigoTaxista, t.personas.nombres,t.personas.apellidos, t.idTaxista,
+                                                             t.longitudActual, t.latitudActual, t.estadodisponibilidad.estadoDisponibilidad,
+                                                             t.personas.contactos.telefonoMovil, t.rating, t.personas.foto};
+
+                return Json(taxistas.ToList(), JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
 
         //vista de mapa
         public ActionResult Index()
